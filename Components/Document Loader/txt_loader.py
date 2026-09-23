9cc3570486b56902c6f860b2cd3e3657de6474da
Muhammad-Hunain-Official <hunainejaz200@gmail.com>
2026-09-23 23:18:53 +0500

    Working

---
+++ Components/Document Loader/txt_loader.py
@@ -0,0 +1,16 @@
+from langchain_community.document_loaders import TextLoader
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import PromptTemplate   
+from dotenv import load_dotenv
+load_dotenv()
+model = ChatGoogleGenerativeAI( model="gemini-2.5-flash",temperature=0.2, max_output_tokens=512)
+prompt = PromptTemplate(
+    template = "Write the summary of the following text:\n{text}\n\nSummary:",
+    input_variables = ["text"],
+    )
+parser = StrOutputParser()
+loader = TextLoader("Questions.txt")
+docs = loader.load()
+chain = prompt | model | parser 
+print(chain.invoke({'text':docs[0].page_content}))
